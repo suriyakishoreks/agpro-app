13a02ae6219ca8f4428a86b0690cdfdde8571852
suriyakishoreks <kssuriyakishore16@gmail.com>
2021-12-24 15:33:05 +0530

    Header Progress

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,20 +1,24 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 // styles
 import logo from '../../assets/AgPro.svg';
 import settingIcon from '../../assets/settings.svg';
 import styles from '../../styles/components/ui/Header.module.scss';
 
 export default function Header() {
+
+   const location = useLocation();
+
    return (
       <div className={styles.headerContainer}>
-         <div className={styles.logoContainer}>
+         <Link className={styles.logoContainer} to="/">
             <img src={logo} alt="logo" height="35px" width="35px" />
             <h1>AgPro</h1>
-         </div>
+         </Link>
          <div className={styles.menuContainer}>
             <nav>
-               <NavItem title="Projects" />
-               <NavItem title="Calendar" />
+               <NavItem title="Projects" route="/projects" active={location?.pathname?.includes('/projects')} />
+               <NavItem title="Calendar" route="/calendar" active={location?.pathname?.includes('/calendar')} />
                <button className={styles.taskButton}>Create Task</button>
             </nav>
             <button>
@@ -25,11 +29,11 @@ export default function Header() {
    );
 }
 
-function NavItem({ title }) {
+function NavItem({ title, route, active }) {
    return (
       <div className={styles.navItem}>
-         <h1>{title}</h1>
-         {/* <div className={styles.navBottomBar}></div> */}
+         <Link className={styles.navLink} to={route}>{title}</Link>
+         <div style={active ? {} : { width: 0 }} className={styles.navBottomBar}></div>
       </div>
    );
 }
